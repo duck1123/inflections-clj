327beba421c0ebe56baa52aa107f3d3c236a6540
Daniel E. Renfer <duck@kronkltd.net>
2011-07-23 06:46:20 -0400

    update to clojure 1.3

--- project.clj
+++ project.clj
@@ -3,6 +3,7 @@
   :autodoc {:copyright "Copyright (c) 2011 Roman Scherer" :name "inflections"}
   :description "Rails-like inflections for Clojure."
   :url "http://github.com/r0man/inflections-clj"
-  :dependencies [[org.clojure/clojure "1.2.1"]
-                 [org.clojure/clojure-contrib "1.2.0"]]
-  :dev-dependencies [[org.clojars.rayne/autodoc "0.8.0-SNAPSHOT"]])
+  :dependencies [[org.clojure/clojure "1.3.0-beta1"]
+                 [org.clojure.contrib/seq "1.3.0-alpha4"]]
+  :dev-dependencies [[org.clojars.rayne/autodoc "0.8.0-SNAPSHOT"]
+                     [swank-clojure "1.3.0-SNAPSHOT"]])
